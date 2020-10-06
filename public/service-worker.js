696745270429d53010b6a7190ad6d82ab5777942
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-06 19:04:24 -0400

    Updated files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const DATA_CACHE_NAME = "data-cache-v4";
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
-    ".index.js",
+    "./index.js",
     "style.css",
     "./icons/icon-192x192.png",
     "./icons/icon-512x512.png",
