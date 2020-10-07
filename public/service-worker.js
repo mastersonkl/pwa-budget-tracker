8d2edd1d33073d00430284fc6e3c8ac56fe77a5a
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-06 20:12:08 -0400

    updated files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./index.js",
-    "style.css",
+    "./style.css",
     "./icons/icon-192x192.png",
     "./icons/icon-512x512.png",
 ];
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 const app = express();
 
@@ -28,3 +28,4 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
